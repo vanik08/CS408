5e77c7127cb1bfc17f59907af8626211a4bdaf85
vanik08 <vanik08@gmail.com>
2015-05-10 21:58:22 -0700

    Simplified backend

--- server/app.js
+++ server/app.js
@@ -1,20 +1,18 @@
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express    = require('express');        
+var app        = express();                 
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 9000;        
+var port = 9000;        
 
 require("./routes")(app);
-
 
 //DATABASE 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/myapp');
 
-// var Thing = require('./api/Thing/thingSchema');
 
 // START THE SERVER
 app.listen(port);
